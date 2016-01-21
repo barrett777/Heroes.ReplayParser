bf8a0d344496619d912189795cb1a0cfe0ffa58f
Ben Barrett <barrett777@gmail.com>
2016-01-20 16:29:48 -0800

    Using a more precise build number from the header

--- Heroes.ReplayParser/mpq.header/MpqHeader.cs
+++ Heroes.ReplayParser/mpq.header/MpqHeader.cs
@@ -38,15 +38,22 @@
             // [0] = Blob, "Heroes of the Storm replay 11" - Strange backward arrow before 11 as well.  I don't think the '11' will change, as I believe it was also always '11' in Starcraft 2 replays.
 
             replay.ReplayVersion = string.Format("{0}.{1}.{2}.{3}", headerStructure.dictionary[1].dictionary[0].vInt.Value, headerStructure.dictionary[1].dictionary[1].vInt.Value, headerStructure.dictionary[1].dictionary[2].vInt.Value, headerStructure.dictionary[1].dictionary[3].vInt.Value);
+            
             replay.ReplayBuild = (int)headerStructure.dictionary[1].dictionary[4].vInt.Value;
 
-            // [2] = VInt, Default 2
+            if (replay.ReplayBuild >= 39951)
+                // 'm_dataBuildNum' may have always been incremented for these smaller 'hotfix' patches, but build 39951 is the first time I've noticed where a Hero's available talent selection has changed in one of these smaller patches
+                // We probably want to use this as the most precise build number from now on
+                replay.ReplayBuild = (int)headerStructure.dictionary[6].vInt.Value;
 
-            replay.Frames = (int)headerStructure.dictionary[3].vInt.Value;
+            // [2] = VInt, Default 2 - m_type
 
-            // [4] = VInt, Default 0
-            // [5] = Depending on replay build, either Blob with gibberish, or array of 16 bytes (basically a Blob), also with gibberish.  Of ~770 pre-wipe replays, there were only 11 distinct blobs, so this is likely a map version hash or something
-            // [6] = Replay Build (Same as what is in [1])
+            replay.Frames = (int)headerStructure.dictionary[3].vInt.Value; // m_elapsedGameLoops
+
+            // [4] = VInt, Default 0 - m_useScaledTime
+            // [5] = Depending on replay build, either Blob with gibberish, or array of 16 bytes (basically a Blob), also with gibberish.  Of ~770 pre-wipe replays, there were only 11 distinct blobs, so this is likely a map version hash or something - 'm_ngdpRootKey'
+            // [6] = Replay Build (Usually the same as what is in [1], but can be incremented by itself for smaller 'hotfix' patches) - m_dataBuildNum
+            // [7] = m_fixedFileHash
         }
     }
 }
