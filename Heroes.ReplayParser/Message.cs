6534e294fb2ae683393ce5d2dcbe4f788465ab21
Ben Barrett <barrett777@gmail.com>
2016-07-18 12:55:38 -0700

    Rare bug fix v2

--- Heroes.ReplayParser/Message.cs
+++ Heroes.ReplayParser/Message.cs
@@ -26,14 +26,20 @@ namespace Heroes.ReplayParser
             }
             else if (PingMessage != null)
             {
-                if (!string.IsNullOrEmpty(MessageSender.Character))
+                if (MessageSender == null)
+                    // I've seen this in at least one replay, and I think it happens right before or after a player is disconnecting or reconnecting
+                    return $"({Timestamp}) [{PingMessage.MessageTarget}] ((Unknown)) used a ping";
+                else if (!string.IsNullOrEmpty(MessageSender.Character))
                     return $"({Timestamp}) [{PingMessage.MessageTarget}] {MessageSender.Name} ({MessageSender.Character}) used a ping";
                 else
                     return $"({Timestamp}) [{PingMessage.MessageTarget}] {MessageSender.Name} used a ping";
             }
             else if (PlayerAnnounceMessage != null)
             {
-                if (!string.IsNullOrEmpty(MessageSender.Character))
+                if (MessageSender == null)
+                    // I've seen this in at least one replay, and I think it happens right before or after a player is disconnecting or reconnecting
+                    return $"({Timestamp}) ((Unknown)) announced {PlayerAnnounceMessage.AnnouncementType}";
+                else if (!string.IsNullOrEmpty(MessageSender.Character))
                     return $"({Timestamp}) {MessageSender.Name} ({MessageSender.Character}) announced {PlayerAnnounceMessage.AnnouncementType}";
                 else
                     return $"({Timestamp}) {MessageSender.Name} announced {PlayerAnnounceMessage.AnnouncementType}";
