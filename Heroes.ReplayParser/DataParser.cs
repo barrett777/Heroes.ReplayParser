c440481685c645ccee3e295f86596c07d66a1d5c
Ben Barrett <barrett777@gmail.com>
2016-08-11 14:39:22 -0700

    For now, set the default Battlelobby parse to just grab BattleTags, until I can dig into this more.

--- Heroes.ReplayParser/DataParser.cs
+++ Heroes.ReplayParser/DataParser.cs
@@ -145,7 +145,8 @@ namespace Heroes.ReplayParser
 
             // Replay Server Battlelobby
             if (!ignoreErrors)
-                ReplayServerBattlelobby.Parse(replay, GetMpqFile(archive, ReplayServerBattlelobby.FileName));
+                ReplayServerBattlelobby.GetBattleTags(replay, GetMpqFile(archive, ReplayServerBattlelobby.FileName));
+                // ReplayServerBattlelobby.Parse(replay, GetMpqFile(archive, ReplayServerBattlelobby.FileName));
 
             // Parse Unit Data using Tracker events
             Unit.ParseUnitData(replay);
