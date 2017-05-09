39329c369f606dced88fa60a76559f2166c7cd89
Ben Barrett <barrett777@gmail.com>
2017-05-08 19:42:56 -0700

    Fix parsing 'Checkpoint: Hanamura'
    This fixes any future replays that do not have a specific tracker event related to game start.  Probably will only happen in special Brawl games
    
    Fixes #45

--- Heroes.ReplayParser/Statistics.cs
+++ Heroes.ReplayParser/Statistics.cs
@@ -106,9 +106,16 @@ namespace Heroes.ReplayParser
 
             var playerIDTalentIndexDictionary = new Dictionary<int, int>();
 
-            foreach (var trackerEvent in replay.TrackerEvents.Where(i => i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.UpgradeEvent || i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.StatGameEvent || i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.ScoreResultEvent))
+            foreach (var trackerEvent in replay.TrackerEvents.Where(i =>
+				i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.PlayerSetupEvent ||
+				i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.UpgradeEvent ||
+				i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.StatGameEvent ||
+				i.TrackerEventType == ReplayTrackerEvents.TrackerEventType.ScoreResultEvent))
                 switch (trackerEvent.TrackerEventType)
                 {
+					case ReplayTrackerEvents.TrackerEventType.PlayerSetupEvent:
+						playerIDDictionary[(int)trackerEvent.Data.dictionary[0].vInt.Value] = replay.ClientListByWorkingSetSlotID[(int)trackerEvent.Data.dictionary[3].optionalData.vInt.Value];
+						break;
                     case ReplayTrackerEvents.TrackerEventType.UpgradeEvent:
                         switch (trackerEvent.Data.dictionary[1].blobText)
                         {
