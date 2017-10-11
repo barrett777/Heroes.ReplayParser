4c714789f26bdbe4b5d530168fbcca34f9975c1f
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-10-10 21:51:19 -0500

    Added BZip2 and Zlib compression code to project

--- MpqTool/Foole.Mpq/MpqHuffman.cs
+++ MpqTool/Foole.Mpq/MpqHuffman.cs
@@ -295,9 +295,11 @@ namespace Foole.Mpq
 				LinkedNode child1 = current.Prev;
 				if (child1 == null) break;
 
-				LinkedNode parent = new LinkedNode(0, child0.Weight + child1.Weight);
-				parent.Child0 = child0;
-				child0.Parent = parent;
+                LinkedNode parent = new LinkedNode(0, child0.Weight + child1.Weight)
+                {
+                    Child0 = child0
+                };
+                child0.Parent = parent;
 				child1.Parent = parent;
 
 				current.Insert(parent);
@@ -311,13 +313,17 @@ namespace Foole.Mpq
 			LinkedNode parent = tail;
 			LinkedNode result = tail.Prev; // This will be the new tail after the tree is updated
 
-			LinkedNode temp = new LinkedNode(parent.DecompressedValue, parent.Weight);
-			temp.Parent = parent;
+            LinkedNode temp = new LinkedNode(parent.DecompressedValue, parent.Weight)
+            {
+                Parent = parent
+            };
 
-			LinkedNode newnode = new LinkedNode(decomp, 0);
-			newnode.Parent = parent;
+            LinkedNode newnode = new LinkedNode(decomp, 0)
+            {
+                Parent = parent
+            };
 
-			parent.Child0 = newnode;
+            parent.Child0 = newnode;
 
 			tail.Next = temp;
 			temp.Prev = tail;
