b9250351138e6f15eabc7a7c1fda26b785c2e7b7
Ben Barrett <barrett777@gmail.com>
2021-05-18 15:54:33 -0700

    Update to handle replay build 85027 (HotS Overwatch Cosplay event)

--- Heroes.ReplayParser/MPQFiles/ReplayInitData.cs
+++ Heroes.ReplayParser/MPQFiles/ReplayInitData.cs
@@ -195,6 +195,21 @@ namespace Heroes.ReplayParser.MPQFiles
                 reader.ReadBoolean(); // m_isPremadeFFA
                 reader.ReadBoolean(); // m_isCoopMode
 
+                if (replay.ReplayBuild >= 85027)
+                {
+                    reader.ReadBoolean(); // m_isRandomTestValue
+
+                    // m_disabledHeroList
+                    var disabledHeroListLength = reader.Read(10);
+
+                    // TODO: This has been '0' in all my games, so nothing to see here
+                    // I'm not sure what it is for.  Maybe it is just for ranked games, or something yet to be implemented in the game client
+                    for (var i = 0; i < disabledHeroListLength; i++)
+                    {
+                        var disabledHero = new string(BitConverter.GetBytes(reader.Read(32)).Select(k => (char)k).Reverse().ToArray());
+                    }
+                }
+
                 #region m_lobbyState
 
                 reader.Read(3); // m_phase
